68712c3a4bf572f1ea3eef65f23b3190f9557ede
Dony Arisandy Wiranata <donyarisandy@googlemail.com>
2017-04-11 19:49:00 +0700

    Update main.py
    Adding missing import

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import nltk
-
 # Dependensi untuk nltk.download() :
 #   DO THIS BELOW FIRST
 # a. download NLTK
@@ -11,6 +9,7 @@ import re
 from nltk.corpus import brown
 from nltk.probability import *
 from nltk.corpus import wordnet
+from nltk import sent_tokenize, word_tokenize
 
 
 # 1. menghitung frekuensi dari korpus BROWN
@@ -27,46 +26,31 @@ def freq(word):
 string = input("Enter a string :\n")
 
 # tokenisasi menjadi kalimat
-sents = nltk.sent_tokenize(string)
-#print(sents)
+sents = sent_tokenize(string)
 
 # tokenisasi kata
 final_word = {}
 for sent in sents:
-	# $print(sent)
-	tokens = nltk.word_tokenize(sent)
+	tokens = word_tokenize(sent)
 	# pemeringkatan top sekian infrequent token
 	freqToken = [None]*len(tokens)
 	for index, token in enumerate(tokens):
 	 	freqToken[index] = freq(token)
-	# print(tokens)
-	# print(freqToken)
 	sortedtokens = [f for (t,f) in sorted(zip(freqToken,tokens))]
 	n = int(0.3 * len(tokens))
-	#print(n)
 	difficultWords = []
 	for i in range(0, n):
 		difficultWord = sortedtokens[i]
 		difficultWords.append(difficultWord)
-		#print("kata susah ", difficultWord)
-		#print(wordnet.synsets(difficultWord))
 		calon_pengganti = {}
 		for synset in wordnet.synsets(difficultWord):
 			for lemma in synset.lemmas():
 				calon_pengganti[lemma.name()] = freq(lemma.name())
-		#$print(calon_pengganti)
 		if len(calon_pengganti) > 0 :
 			final_word[difficultWord] = max(calon_pengganti, key=lambda i: calon_pengganti[i])
-	#print(final_word)		
-	# $print(tokens)
+
 	for token in tokens:
-		# $print(token)
-		# if idx != len(tokens):
-		# 	print(idx, len(tokens))
-		# 	if not(re.match('[a-zA-Z0-9]',token[idx+1])):
-		# 		print('hmm')
 		if token in difficultWords and token in final_word:
 			print(final_word[token], end=' ')
 		else:
 			print(token, end=' ')
-	# $print()
